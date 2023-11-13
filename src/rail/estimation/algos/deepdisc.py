3d52dce938d33f463194112d1f692e17225a1988
grantmerz <grantmerz97@gmail.com>
2023-11-13 10:34:42 -0600

    minimal working example

--- src/rail/estimation/algos/deepdisc.py
+++ src/rail/estimation/algos/deepdisc.py
@@ -1,4 +1,29 @@
+import numpy as np
 from rail.estimation.estimator import CatEstimator, CatInformer
+from rail.core.data import TableHandle
+from detectron2.config import LazyConfig, get_cfg
+import detectron2.solver as solver
+import detectron2.data as data
+from deepdisc.data_format.augment_image import train_augs
+from deepdisc.data_format.image_readers import DC2ImageReader
+from deepdisc.data_format.register_data import register_data_set, register_loaded_data_set
+from deepdisc.model.loaders import (
+    RedshiftFlatDictMapper,
+    return_test_loader,
+    return_train_loader,
+)
+from deepdisc.model.models import return_lazy_model
+from deepdisc.training.trainers import (
+    return_evallosshook,
+    return_lazy_trainer,
+    return_optimizer,
+    return_savehook,
+    return_schedulerhook,
+)
+from rail.deepdisc.configs import *
+from deepdisc.inference.match_objects import get_matched_object_classes_new, get_matched_z_pdfs_new, get_matched_z_points_new
+from deepdisc.inference.predictors import return_predictor_transformer
+
 
 class DeepDiscInformer(CatInformer):
     """Placeholder for informer stage class"""
@@ -10,15 +35,112 @@ class DeepDiscInformer(CatInformer):
         CatInformer.__init__(self, args, comm=comm)
 
     def run(self):
-        pass
+        """
+        Train a inception NN on a fraction of the training data
+        """
+        # Not sure what .get_data('input') is ?
+        train_data = self.get_data('input')
+        
+        cfgfile = self.config.cfgfile
+        batch_size = self.config.batch_size
+        numclasses = self.config.numclasses
+        epochs = self.config.epochs
+        output_dir = self.config.output_dir
+        output_name = self.config.output_name
+        
+        val_per = 5
+
+        cfg = get_lazy_config(cfgfile, batch_size, numclasses)
+        cfg_loader = get_loader_config(output_dir, batch_size, epochs)
+        
+        
+        model = return_lazy_model(cfg)
+        cfg.optimizer.params.model = model
+        cfg.optimizer.lr = 0.001
+        #optimizer = return_optimizer(cfg)
+        optimizer = solver.build_optimizer(cfg_loader, model)
+
+        
+        
+        mapper = RedshiftFlatDictMapper().map_data
+        loader = data.build_detection_train_loader(train_data,mapper=mapper,total_batch_size=batch_size)
+        test_loader = data.build_detection_test_loader(train_data,mapper=mapper,batch_size=batch_size)
+
+        saveHook = return_savehook(output_name)
+        lossHook = return_evallosshook(val_per, model, test_loader)
+        schedulerHook = return_schedulerhook(optimizer)
+        hookList = [lossHook, schedulerHook, saveHook]
+
+        trainer = return_lazy_trainer(model, loader, optimizer, cfg, cfg_loader, hookList)
+
+        trainer.set_period(5)
+
+        print("Model training:")
+        trainer.train(0, epochs)
+
+        self.model = dict(nnmodel=model)
+        self.add_data('model', self.model)
 
 
+        
 class DeepDiscEstimator(CatEstimator):
-    name = "DeepDiscEstimator"
+    """DeepDISC estimator
+    """
+    name = 'DeepDiscEstimator'
     config_options = CatEstimator.config_options.copy()
-
+    config_options.update()
+    
+    outputs = [('output', TableHandle)]
+    
     def __init__(self, args, comm=None):
+        """ Constructor:
+        Do Estimator specific initialization """
+        
+        self.nnmodel = None
         CatEstimator.__init__(self, args, comm=comm)
+        
+    
+    def open_model(self, **kwargs):
+        CatEstimator.open_model(self, **kwargs)
+        if self.model is not None:
+            self.nnmodel = self.model['nnmodel']
 
-    def _process_chunk(self, start, end, data, first):
-        pass
+    def run(self):
+        test_data = self.get_data('input')
+        
+        cfgfile = self.config.cfgfile
+        batch_size = self.config.batch_size
+        numclasses = self.config.numclasses
+        epochs = self.config.epochs
+        output_dir = self.config.output_dir
+        output_name = self.config.output_name
+        
+        
+        cfg = get_lazy_config(cfgfile, batch_size, numclasses)
+        cfg_loader = get_loader_config(output_dir, batch_size, epochs)
+        
+        cfg.train.init_checkpoint = os.path.join(output_dir,output_name)+'.pth'
+        
+        # Process test images same way as training set
+        predictor = return_predictor_transformer(cfg, cfg_loader)
+        mapper = RedshiftFlatDictMapper().map_data
+
+        print('Processing Data')
+        dataset_dicts={}
+        dds = []
+        for row in test_data:
+            dds.append(mapper(row))
+        dataset_dicts['test'] = dds
+        
+        
+        print("Matching objects")
+        true_classes, pred_classes = get_matched_object_classes_new(dataset_dicts["test"],  predictor)
+        self.true_zs, self.preds = get_matched_z_points_new(dataset_dicts["test"], predictor)
+        #self.pred = self.preds.squeeze()
+        
+    def finalize(self):
+        preds = np.array(self.preds)
+        self.add_handle('output', data=preds)
+
+        
+        
