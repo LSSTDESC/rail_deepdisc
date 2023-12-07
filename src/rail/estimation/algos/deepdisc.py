cab7ccb59837a86958a11d9241c37705d5608133
Olivia R. Lynn <olynn@andrew.cmu.edu>
2023-12-07 17:06:14 -0500

    Update deepdisc.py

--- src/rail/estimation/algos/deepdisc.py
+++ src/rail/estimation/algos/deepdisc.py
@@ -268,7 +268,7 @@ class DeepDiscPDFEstimator(CatEstimator):
         cfg.train.init_checkpoint = os.path.join(output_dir, output_name) + ".pth"
 
         self.predictor = return_predictor_transformer(cfg, cfg_loader)
-      
+
         # Process test images same way as training set
         mapper = RedshiftDictMapper(
             DC2ImageReader(), lambda dataset_dict: dataset_dict["filename"]
