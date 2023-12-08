7ce0c4e3245d144c6cb5b411c64cf844f115bff4
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-12-07 16:12:03 -0800

    Fixing height/width ordering for reshape.

--- src/rail/estimation/algos/deepdisc.py
+++ src/rail/estimation/algos/deepdisc.py
@@ -67,11 +67,10 @@ class DeepDiscInformer(CatInformer):
         for start_idx, _, chunk in itr:
             for idx, image in enumerate(chunk['images']):
                 this_img_metadata = metadata[start_idx + idx]
-                height = this_img_metadata["height"]
-                width = this_img_metadata["width"]
+                image_height = this_img_metadata["height"]
+                image_width = this_img_metadata["width"]
 
-                #! This spot could have bugs width, height, or height, width???
-                reformed_image = image.reshape(6, width, height).astype(np.float32)
+                reformed_image = image.reshape(6, image_height, image_width).astype(np.float32)
 
                 filename = f'image_{start_idx + idx}.npy'
                 file_path = os.path.join(self.temp_dir, filename)
@@ -243,11 +242,10 @@ class DeepDiscPDFEstimator(CatEstimator):
         for start_idx, _, chunk in itr:
             for idx, image in enumerate(chunk['images']):
                 this_img_metadata = metadata[start_idx + idx]
-                height = this_img_metadata["height"]
-                width = this_img_metadata["width"]
+                image_height = this_img_metadata["height"]
+                image_width = this_img_metadata["width"]
 
-                # Note well: the predictor assumes a different image shape than the informer. 
-                reformed_image = image.reshape(6, width, height).astype(np.float32)
+                reformed_image = image.reshape(6, image_height, image_width).astype(np.float32)
 
                 filename = f'image_{start_idx + idx}.npy'
                 file_path = os.path.join(self.temp_dir, filename)
